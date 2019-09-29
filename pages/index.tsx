70ba5b6fe65d9c31d62655f57aba51b877bea40b
william ricardo ariza velez <wilarizav@gmail.com>
2019-09-29 08:12:30 -0500

    chore: base project

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+const Index: React.FunctionComponent = () => {
+    return (
+        <Layout title="Lifit">
+            <h1>Lifit</h1>
+        </Layout>
+    )
+}
+export default Index
